a5efca3aca51c110ef3328a3976bd0a5e9f8fd14
Marcus Hellberg <marcus@vaadin.com>
2025-03-06 13:11:30 -0500

    do a slightly better job at handling inputs

--- src/main/java/org/vaadin/lumoava/LumoThemeGenerator.java
+++ src/main/java/org/vaadin/lumoava/LumoThemeGenerator.java
@@ -1,15 +1,16 @@
 package org.vaadin.lumoava;
 
-import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.upload.Upload;
-import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
-import com.vaadin.flow.router.Route;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.util.MimeTypeUtils;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
+import com.vaadin.flow.router.Route;
 
 @Route("")
 public class LumoThemeGenerator extends VerticalLayout {
@@ -29,7 +30,7 @@ public class LumoThemeGenerator extends VerticalLayout {
         var ui = UI.getCurrent();
 
         upload.addSucceededListener(e -> {
-            var css = chatClient.prompt()
+            chatClient.prompt()
                 .user(userMessage -> userMessage
                     .text("""
                         Use the uploaded image to generate a Lumo theme using the following documentation as a guideline:
@@ -39,7 +40,9 @@ public class LumoThemeGenerator extends VerticalLayout {
                         {documentation}
                         ====
                         Create a light and a dark variant.
-                        Only use Lumo variables and Vaadin HTML elements, don't write custom CSS classes.
+                        Only use Lumo variables and Vaadin HTML element selectors, don't write custom CSS classes or properties.
+                        Pay special attention to input fields and buttons. Use an appropriate border radius, width, and color.
+                        Use the common `--vaadin-input-field` prefixed properties to update all input fields in a coherent manner.                      
                         Output ONLY contents of the CSS file, nothing else.
                         """).param("documentation", lumoDocs)
                     .media(MimeTypeUtils.parseMimeType(e.getMIMEType()), new InputStreamResource(buffer.getInputStream())))
